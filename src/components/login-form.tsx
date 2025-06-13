c51ead31b2889e925d3a30e99ae857ff9aecd80a
Pedro Monteiro <pedro.oliveira@monteirodev.com>
2025-06-13 14:01:05 -0300

    feat: add login form component with GitHub and Google authentication options

---
+++ src/components/login-form.tsx
@@ -0,0 +1,109 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+import GitHubIcon from './icons/github'
+import GoogleIcon from './icons/google'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from './ui/form'
+
+export function LoginForm() {
+  const loginFormSchema = z.object({
+    email: z
+      .string()
+      .email({ message: 'Email inválido' })
+      .min(1, { message: 'Email é obrigatório' }),
+    password: z.string().min(1, { message: 'Senha é obrigatória' }),
+  })
+
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Entre na sua conta</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Digite seu email abaixo para entrar na sua conta
+          </p>
+        </div>
+        <div className="grid gap-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="m@exemplo.com" type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <div className="flex items-center">
+                  <FormLabel>Senha</FormLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueceu sua senha?
+                  </a>
+                </div>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            Entrar
+          </Button>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+            <span className="text-muted-foreground relative z-10 bg-slate-100 px-2">
+              Ou continue com
+            </span>
+          </div>
+          <Button variant="outline" className="w-full">
+            <GitHubIcon />
+            Entrar com GitHub
+          </Button>
+          <Button variant="outline" className="w-full">
+            <GoogleIcon />
+            Entrar com Google
+          </Button>
+        </div>
+        <div className="text-center text-sm">
+          Não tem uma conta?{' '}
+          <a href="#" className="underline underline-offset-4">
+            Cadastre-se
+          </a>
+        </div>
+      </form>
+    </Form>
+  )
+}
